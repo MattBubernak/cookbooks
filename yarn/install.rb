ba597ccc3035a96cd7255fd8c2bed6420ee7fb78
Matt Bubernak <matt.bubernak@gmail.com>
2019-01-24 21:55:43 -0700

    add yarn

---
+++ yarn/install.rb
@@ -0,0 +1,13 @@
+require 'aws-sdk'
+
+node[:deploy].each do |application, deploy|
+
+  apt_repository 'yarn' do
+    uri          'https://dl.yarnpkg.com/debian/'
+    components   ['stable', 'main']
+    key          'https://dl.yarnpkg.com/debian/pubkey.gpg'
+  end
+
+  package 'yarn'
+
+end
